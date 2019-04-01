112be506c22ca5e98e2428f7862e924e8510fa91
lipengcheng <Pengcheng.Li4@geely.com>
2019-04-01 15:31:11 +0800

    commit

--- spring-websocket/src/main/java/com/example/websocket/SpringWebsocketApplication.java
+++ spring-websocket/src/main/java/com/example/websocket/SpringWebsocketApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages = "com.example.websocket")
-public class SpringWebscoketApplication {
+public class SpringWebsocketApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringWebscoketApplication.class, args);
+		SpringApplication.run(SpringWebsocketApplication.class, args);
 	}
 
 }
